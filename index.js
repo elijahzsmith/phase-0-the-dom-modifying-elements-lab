1ae17dacfa8b34a9df44603869cf0859f8165bce
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-17 20:38:12 -0500

    commit tree 4449a8331db5b692cd35bb67cc91e1da10fe02a5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainTag = document.querySelector("main");
+
+mainTag.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "YOUR-NAME is the champion"
